c7a55aaba7c8efa76ee84216716b09aa7aed9b59
Faiz Saiyad <81345756+Enhancifire@users.noreply.github.com>
2022-01-31 11:46:51 +0530

    Setup Emmet Language Server

--- lua/lsp/cmp-config.lua
+++ lua/lsp/cmp-config.lua
@@ -16,7 +16,7 @@ cmp.setup({
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
     end,
--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,17 +1,49 @@
 local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require'lspconfig'
+local configs = require'lspconfig/configs'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+if not lspconfig.emmet_ls then
+  configs.emmet_ls = {
+    default_config = {
+      cmd = {'emmet-ls', '--stdio'};
+      filetypes = {'html', 'css', 'blade'};
+      root_dir = function(fname)
+	return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+  }
+end
+lspconfig.emmet_ls.setup{ capabilities = capabilities; }
 
 lsp_installer.on_server_ready(function(server)
   local opts = {}
-    if server.name == "sumneko_lua" then
-      opts = {
-	settings = {
-	  Lua = {
-	    diagnostics = {
-	      globals = { 'vim', 'use' }
-	    },
-	  }
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+	Lua = {
+	  diagnostics = {
+	    globals = { 'vim', 'use' }
+	  },
 	}
       }
+    }
   end
+
+  if server.name == "emmet_ls" then
+    default_config = {
+      cmd = {'emmet-ls', '--stdio'};
+      filetypes = {'html', 'css', 'blade'};
+      root_dir = function(fname)
+	return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+
+  end
+
   server:setup(opts)
 end)
